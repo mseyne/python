638101927e5f89239296ec111ebd6d2121b3fd67
Michaël Seyne <michaelseyne@gmail.com>
2015-11-05 08:26:40 +0100

    + cs50 week0

--- cs50-course/week0/binary.py
+++ cs50-course/week0/binary.py
@@ -1,63 +1,70 @@
-#binary to decimal converter
+# -*- coding:Utf-8 -*-
+'binary to decimal converter'
 
 from tkinter import *
 import sys
 
-BINARY = 
-[1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 20
-97152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648, 42949672
-96, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472, 274877906944, 549755813888, 1099511627776, 21990232
-55552, 4398046511104, 8796093022208, 17592186044416, 35184372088832, 70368744177664, 140737488355328, 281474976710656, 5
-62949953421312, 1125899906842624, 2251799813685248, 4503599627370496, 9007199254740992, 18014398509481984, 3602879701896
-3968, 72057594037927936, 144115188075855872, 288230376151711744, 576460752303423488, 1152921504606846976, 23058430092136
-93952, 4611686018427387904, 9223372036854775808, 18446744073709551616, 36893488147419103232, 73786976294838206464, 14757
-3952589676412928, 295147905179352825856, 590295810358705651712, 1180591620717411303424, 2361183241434822606848, 47223664
-82869645213696, 9444732965739290427392, 18889465931478580854784, 37778931862957161709568, 75557863725914323419136, 15111
-5727451828646838272, 302231454903657293676544, 604462909807314587353088, 1208925819614629174706176, 24178516392292583494
-12352, 4835703278458516698824704, 9671406556917033397649408, 19342813113834066795298816, 38685626227668133590597632, 773
-71252455336267181195264, 154742504910672534362390528, 309485009821345068724781056, 618970019642690137449562112, 12379400
-39285380274899124224, 2475880078570760549798248448, 4951760157141521099596496896, 9903520314283042199192993792, 19807040
-628566084398385987584, 39614081257132168796771975168, 79228162514264337593543950336, 158456325028528675187087900672, 316
-912650057057350374175801344, 633825300114114700748351602688, 1267650600228229401496703205376]
+BINARY = [1]
 
+def createBinaryList():
+	"I used it to find the BINARY list"
+	global BINARY
+	b = 2
+	c = 0
+	while c < 100:
+		BINARY.append(b)
+		c += 1
+		b = b + b
 
-# def createBinaryList():
-# 	"I used it to find the BINARY list"
-# 	global BINARY
-# 	b = 2
-# 	c = 0
-# 	while c < 100:
-# 		BINARY.append(b)
-# 		c += 1
-# 		b = b + b
-# 	print(BINARY)
+def convertToBinary():
+	dec = ent1.get()
+	can1.itemconfig(text1, text=dec)
 
+	#fonction to check if the number is composed only of number
 
-def convertBinary():
-	pass
+def convertToDecimal():
+	bin = ent1.get()
+	can1.itemconfig(text1, text=bin)
 
-def convertDecimal():
-	pass
+	#fonction to check if the number is binary (composed of 0 and 1)
 
 def exit(event):
 	sys.exit()
 
-fenetre = Tk()
-fenetre.bind('<Escape>', exit)
+def canvas():
+	return Canvas(fenetre, width=200, height=50, bg="light grey")
 
-createBinaryList()
+def texte():
+	return can1.create_text(100, 25, text="Convertissez une valeur décimal\n en valeur binaire, ou l'inverse.")
 
-can1 = Canvas(fenetre, width=300, height=100)
-can1.pack()
+def entree():
+	return Entry(fenetre, width=10)
 
-ent1 = Entry(fenetre)
+def boutons():
+	bouQ = Button(fenetre, command=fenetre.quit, text="Q")
+	bouQ.grid(row=2, column=4, padx=5, pady=5)
+	bouD = Button(fenetre, command=convertToDecimal, text="To decimal")
+	bouD.grid(row=2, column=2)
+	bouB = Button(fenetre, command=convertToBinary, text="To binary")
+	bouB.grid(row=2, column=3)
 
-ent1.pack(side="left")
-bouQ = Button(fenetre, command=fenetre.quit, text="Quitter")
-bouQ.pack(side="right")
-bouB = Button(fenetre, command=convertBinary, text="To decimal")
-bouQ.pack(side="right")
-bouB = Button(fenetre, command=convertDecimal, text="To binary")
-bouQ.pack(side="right")
+def grid():
+	can1.grid(row=1, column=1, columnspan=4, pady=10)
+	ent1.grid(row=2, column=1, sticky="w", padx=5, pady=5)
 
-fenetre.mainloop()
+
+if __name__ == '__main__':
+	createBinaryList()
+
+	fenetre = Tk()
+	fenetre.resizable(0,0)
+	fenetre.title("Convertisseur Binaire Décimal")
+	fenetre.bind('<Escape>', exit)
+
+	boutons()
+	ent1 = entree()
+	can1 = canvas()
+	text1 = texte()
+	grid()
+
+	fenetre.mainloop()
