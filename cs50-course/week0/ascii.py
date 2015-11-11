af84840debf0d200aa52505333b4e8552ebf0933
Michaël Seyne <michaelseyne@gmail.com>
2015-11-11 08:02:53 +0100

    + ascii week0

---
+++ cs50-course/week0/ascii.py
@@ -0,0 +1,13 @@
+# -*- coding:Utf:8 -*-
+"ascii/unicode table"
+
+from tkinter import *
+
+TITLE = "ASCII / UNICODE TABLE"
+
+if __name__ == '__main__':
+	root = Tk()
+	root.title(TITLE)
+
+	root.geometry("400x400+400+400")
+	root.mainloop()
