80626c1bee14f349f809fe73c174cfef5cf713b6
Michaël Seyne <michaelseyne@gmail.com>
2015-11-17 08:09:06 +0100

    + caesar dev

--- cs50-course/week2/caesar-vigerene-ciphers.py
+++ cs50-course/week2/caesar-vigerene-ciphers.py
@@ -8,16 +8,24 @@ import os
 #datas
 TITLE = "-----------------------\nCRYPTOGRAPHY ALGORITHMS\n-----------------------"
 
-ALNUM = "ABCDEFGHIJKLMONPQRSTUVWXYZ0123456789"
+ALNUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+ALPHA = "abcdefghijklmnopqrstuvwxyz"
+NUMERIC = "1234567890"
 
-TEXTS = [
+INFO = [
 "What cipher do you want to use ?",
 "1 : The Caesar cipher ?",
 "2 : The Vigenère cipher ?",
 "3 : No, thank you, bye.",
 "Do you want to encode a text or decode a cipher ?",
 "1 : Encode text.",
-"2 : Decode cipher."
+"2 : Decode cipher.",
+"What is the text you wish to encode ?",
+"What is the shift you want to use ? (type 0 to get them all)",
+"What cipher do you want to decode ?",
+"What is the shift of the cipher ? (if you don't know, type 0)",
+"You can use only the alphabetic characters.",
+"You can use only the numerical characters."
 ]
 
 PROMPT = '>>>'
@@ -28,30 +36,77 @@ PROMPT = '>>>'
 
 #fonctions
 def string(limits):
-	"ask for the user to give an input"
+	"ask for the user to give a short input"
 	question = 1
 	while question:
 		answer = input(PROMPT)
 		if answer in list(limits):
 			return answer
 		else:
-			print("Vous pouvez écrire ", end="")
+			print("You can write ", end="")
 			for i in limits:
 				print("'", i, sep="", end="")
 			print(".")
 
 
-def encodeCaesar():
-	"possibility to choose the shift or see the 26 possibilities"
-	print("let's encode caesar cipher!")
-	return False
+def longString(limits, caution):
+	"ask for the user to give a long input"
+	question = 1
+	while question:
+		flag = 1
+		answer = input(PROMPT).lower()
+		for i in answer:
+			if i not in list(limits):
+				flag = 0
+			if i == " ":
+				flag = 1
+		if flag == 1:
+			return answer
+		else:
+			print(caution)
 
-def decodeCaesar():
-	print("let's decode caesar cipher!")
-	return False
+
+def datasCaesar():
+	"get the datas from the user"
+	action = menu2()
+	
+	if action == "1":
+		print(INFO[7])
+		text = longString(ALPHA, INFO[11]) 
+		print(INFO[8])
+		shift = longString(NUMERIC, INFO[12])
+		encodeCaesar(text, shift)
+		return False
+	
+	if action == "2":
+		print(INFO[9])
+		text = longString(ALPHA, INFO[11]) 
+		print(INFO[10])
+		shift = longString(NUMERIC, INFO[12])
+		decodeCaesar(text, shift)
+		return False
+
+def encodeCaesar(text, shift=0):
+	"encode the datas from the user"
+	encoding = []
+	print("your text", text, "your shift", shift)
+
+def decodeCaesar(ciphertext, shift=0):
+	"decode the datas from the user"
+	decoding = []
+	print("your text", ciphertext, "your shift", shift)
 
 def displayCaesar():
 	"print out all the shifts 1: 2: .. 26:"
+
+
+def datasVigenere():
+	"get the datas from user"
+	action = menu2()
+	if action == "1":
+		cipher = encodeVigenere()
+	if action == "2":
+		cipher = decodeVigenere()
 
 def encodeVigenere():
 	print("let's encode Vigenère cipher!")
@@ -61,11 +116,15 @@ def decodeVigenere():
 	print("let's decode Vigenère cipher!")
 	return False
 
+def displayVignere():
+	"display the datas encoded or decoded from the user"
+	pass
+
 def menu2():
 	"ask for encoding or decoding"
-	print(TEXTS[4])
-	print(TEXTS[5])
-	print(TEXTS[6])
+	print(INFO[4])
+	print(INFO[5])
+	print(INFO[6])
 	answer = string('12')
 	return answer
 
@@ -75,26 +134,18 @@ def menu():
 	"ask user a cryptography algorithm"
 	os.system('cls')
 	print(TITLE)
-	print(TEXTS[0])
-	print(TEXTS[1])
-	print(TEXTS[2])
-	print(TEXTS[3])
+	print(INFO[0])
+	print(INFO[1])
+	print(INFO[2])
+	print(INFO[3])
 
 	cipher = True
 	while cipher:
 		answer = string('123Qq')
 		if answer == '1':
-			action = menu2()
-			if action == "1":
-				cipher = encodeCaesar()
-			if action == "2":
-				cipher = decodeCaesar()
+			cipher = datasCaesar()
 		elif answer == '2':
-			action = menu2()
-			if action == "1":
-				cipher = encodeVigenere()
-			if action == "2":
-				cipher = decodeVigenere()
+			cipher = datasVigenere()
 		elif answer in list("3Qq"):
 			sys.exit()
 
